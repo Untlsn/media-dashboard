eeeabdd61d9948e9c5e2adfbeec130a6c9a7c074
Untlsn <Untellson@gmail.com>
2021-02-19 00:57:50 +0100

    Repair alias issue with Typescript

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = function override(config) {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+      '@Libs': path.resolve(__dirname, 'src/libs'),
+      '@Components': path.resolve(__dirname, 'src/components'),
+      '@Style': path.resolve(__dirname, 'src/style')
+    },
+  };
+  return config;
+};
