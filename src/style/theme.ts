11b7f5a6f1f94438a755bcc5981ba13dc71dd7fc
Untlsn <Untellson@gmail.com>
2021-02-19 22:16:37 +0100

    Add Dark and Light Theme template

---
+++ src/style/theme.ts
@@ -0,0 +1,25 @@
+export
+interface ThemeSettings {
+  background: string,
+  secBackground: string,
+  textColor: string,
+  secTextColor: string,
+  tileColor: string
+}
+
+export
+const lightTheme: ThemeSettings = {
+  background: '#fff',
+  secBackground: '#f5f7ff',
+  textColor: '#22283b',
+  secTextColor: '#8f97c6',
+  tileColor: '#f0f2fa'
+}
+export
+const darkTheme: ThemeSettings = {
+  background: '#1e202a',
+  secBackground: '#1f212e',
+  textColor: '#fbfefe',
+  secTextColor: '#8b97c6',
+  tileColor: '#252a41'
+}
---
+++ src/style/themedEl.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export
+const ThemedText = styled.div`
+  color: ${props => props.theme.secTextColor};
+`
+export
+const ThemedBackground = styled.div`
+  background: ${props => props.theme.secBackground};
+`
+export
+const Tile = styled.div`
+  background: ${props => props.theme.tileColor};
+`
