f73b6729bc589ef628086cdaed2a952f9d649c74
Untlsn <Untellson@gmail.com>
2021-02-19 22:13:18 +0100

    Add forgotten Background.ts

---
+++ src/components/Navigation/DarkMode/Switch/Background.ts
@@ -0,0 +1,26 @@
+export default
+class Background {
+  private readonly _off: string
+  private readonly _on?: string
+
+  protected constructor(arg1: string, arg2?: string) {
+    this._off = arg1
+    this._on = arg2
+  }
+
+  get off() { return this._off }
+  get on() { return this._on ?? this._off }
+
+  static mono(always: string) {
+    return new Background(always)
+  }
+  static dual(off: string, on: string) {
+    return new Background(off, on)
+  }
+}
+
+export
+const simpleGradientCreator = (firstColor: string, secondColor: string) => `
+  linear-gradient(90deg, ${firstColor} 0%, ${secondColor} 100%)
+`
+
