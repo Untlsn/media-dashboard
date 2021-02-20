a5da961b7ab8b0655efe76335a097a753ccef74c
Untlsn <Untellson@gmail.com>
2021-02-20 21:41:03 +0100

    Add docs to run on github pages

---
+++ docs/component---src-pages-404-ts-05514190daf035df95ab.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"/S4Z":function(e,n,w){"use strict";w.r(n);var a=w("QeBL");n.default=a.default}}]);
+//# sourceMappingURL=component---src-pages-404-ts-05514190daf035df95ab.js.map
